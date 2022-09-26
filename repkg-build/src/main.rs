2e0d13df50ba2e8f22b6f41320163aefeb8c7f5a
Tristan Fox <tristanfox009@outlook.com>
2022-09-26 14:17:54 +1000

    Add color_eyre

--- repkg-build/src/main.rs
+++ repkg-build/src/main.rs
@@ -1,22 +1,31 @@
 use std::fs::read_to_string;
 
 use clap::{Parser, Subcommand};
+use color_eyre::{eyre::eyre, Result};
+
 use repkg_build::parser::parser;
 
-fn main() {
+fn main() -> Result<()> {
     let cli = Cli::parse();
 
     match cli.command.unwrap_or(Command::Run {
         command: "build".to_string(),
     }) {
         Command::Run { command } => {
-            let content = read_to_string("PACKAGE.repkg").unwrap();
+            let content = read_to_string("PACKAGE.repkg")?;
 
-            let program = parser().parse(content.as_bytes()).unwrap();
+            let program = parser().parse(content.as_bytes())?;
 
-            let _to_exec = program.rules.get(&command.into()).unwrap();
+            dbg!(&program);
+
+            let _to_exec = program
+                .rules
+                .get(&command.into())
+                .ok_or(eyre!("No rules found"))?;
         }
     }
+
+    Ok(())
 }
 
 #[derive(Parser)]
