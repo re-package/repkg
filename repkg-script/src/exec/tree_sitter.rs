0e376a521079587efc6038a99a80245a90d2d4d7
Tristan Fox <tristanfox009@outlook.com>
2022-10-25 14:41:08 +1100

    Add commands and variables

--- repkg-script/src/exec/tree_sitter.rs
+++ repkg-script/src/exec/tree_sitter.rs
@@ -1,6 +1,6 @@
 use std::{collections::BTreeMap, fs, path::Path, str::Utf8Error};
 
-use miette::{bail, Diagnostic, Result};
+use miette::{bail, miette, Diagnostic, Result};
 use thiserror::Error;
 use tree_sitter::{LanguageError, Node, Parser, Tree};
 
@@ -24,6 +24,9 @@ pub enum Error {
     #[error("Missing {}", .0)]
     #[diagnostic(code(repkg_script::missing_field))]
     MissingField(&'static str),
+    #[error("Variable doesnt exist: {}", .0)]
+    #[diagnostic(code(repkg_script::var_doesnt_exist))]
+    VarDoesntExist(String),
 }
 
 pub struct TreeSitterExecutor {
@@ -79,10 +82,15 @@ impl TreeSitterExecutor {
                 self.handle_namespace(node, context)?;
             }
             "command" => {
-                todo!()
+                self.handle_command(node, context)?;
             }
             "comment" => {}
-            a => bail!(UnknownNodeType(a)),
+            "\n" => {}
+            "" => {}
+            a => {
+                dbg!(a);
+                bail!(UnknownNodeType(a))
+            }
         }
         Ok(())
     }
@@ -114,12 +122,97 @@ impl TreeSitterExecutor {
         Ok(())
     }
 
+    fn handle_command(&self, node: Node, context: &mut Context) -> Result<()> {
+        let cmd = self.get_command(node, context)?;
+        context.to_execute.push(cmd);
+
+        Ok(())
+    }
+
+    fn get_command(&self, node: Node, context: &mut Context) -> Result<Command> {
+        let program = node
+            .child_by_field_name("program")
+            .ok_or(MissingField("program"))?;
+        let program = self.handle_path(program, context)?;
+
+        let mut args: Vec<DataType> = vec![];
+        for arg in node.children_by_field_name("args", &mut node.walk()) {
+            let data: DataType = match arg.kind() {
+                "text" => DataType::String(self.node_to_string(arg)?),
+                "variable" => self.get_var(arg, context)?,
+                "command" => DataType::WaitCalc(self.get_command(node, context)?),
+                a => {
+                    dbg!(&a);
+                    bail!(UnknownNodeType(a))
+                }
+            };
+            args.push(data);
+        }
+
+        Ok((program, args))
+    }
+
+    fn handle_path(&self, node: Node, context: &mut Context) -> Result<Vec<String>> {
+        match node.kind() {
+            "nested_identifier" => {
+                let mut first_part = self.handle_path(
+                    node.child_by_field_name("path")
+                        .ok_or(MissingField("path"))?,
+                    context,
+                )?;
+                let mut second_part = self.handle_path(
+                    node.child_by_field_name("name")
+                        .ok_or(MissingField("name"))?,
+                    context,
+                )?;
+
+                first_part.append(&mut second_part);
+                Ok(first_part)
+            }
+            "identifier" => Ok(vec![self.node_to_string(node)?]),
+            a => {
+                bail!(UnknownNodeType(a),)
+            }
+        }
+    }
+
+    fn node_to_string(&self, node: Node) -> Result<String> {
+        Ok(node
+            .utf8_text(self.source.as_bytes())
+            .map_err(Utf8Error)
+            .map(ToString::to_string)?)
+    }
+
     fn get_id(&self, node: Node) -> Result<String> {
         let id = node.child_by_field_name("id").ok_or(MissingField("id"))?;
         Ok(id
             .utf8_text(self.source.as_bytes())
             .map_err(Utf8Error)
             .map(ToString::to_string)?)
+    }
+
+    fn get_var(&self, node: Node, context: &mut Context) -> Result<DataType> {
+        let var = node.child_by_field_name("var").ok_or(MissingField("var"))?;
+
+        let var = match var.kind() {
+            "number" => {
+                let num = self.get_primitive(node)?;
+                match num {
+                    DataType::Number(a) => DataType::Number(a),
+                    _ => {
+                        bail!(miette!("Number must be a number!"))
+                    }
+                }
+            }
+            "percent_sign" => DataType::Wait("args".to_string()),
+            "text" => {
+                let name = self.node_to_string(var)?;
+                context.get(&name).ok_or(VarDoesntExist(name))?.clone()
+            }
+            a => bail!(UnknownNodeType(a)),
+        };
+
+        Ok(var)
     }
 
     fn get_primitive(&self, node: Node) -> Result<DataType> {
@@ -168,17 +261,24 @@ impl TreeSitterExecutor {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum DataType {
     Array(Vec<DataType>),
     String(String),
     Number(usize),
     Context(Context),
+    // Dont calculate variable until runtime.
+    Wait(String),
+    // Calculate command at request time
+    WaitCalc(Command),
 }
 
-#[derive(Default, Debug)]
+type Command = (Vec<String>, Vec<DataType>);
+
+#[derive(Default, Debug, Clone)]
 pub struct Context {
     vars: BTreeMap<String, DataType>,
+    to_execute: Vec<Command>,
 }
 
 impl Context {
@@ -186,8 +286,8 @@ impl Context {
         Self::default()
     }
 
-    pub fn get(&self, string: impl AsRef<String>) -> Option<&DataType> {
-        self.vars.get(string.as_ref())
+    pub fn get(&self, string: &String) -> Option<&DataType> {
+        self.vars.get(string)
     }
 
     pub fn set(&mut self, name: impl ToString, val: DataType) {
