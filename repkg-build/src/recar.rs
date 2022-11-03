5a27d530846532640c1f2abcc73de327e1f673c7
Tristan Fox <tristanfox009@outlook.com>
2022-11-03 18:19:51 +1100

    feat(build/recar): add 'describe' command

--- repkg-build/src/recar.rs
+++ repkg-build/src/recar.rs
@@ -1,4 +1,8 @@
-use std::{fs, path::PathBuf, process};
+use std::{
+    fs::{self, File},
+    path::{Path, PathBuf},
+    process,
+};
 
 use clap::Subcommand;
 use dialoguer::Confirm;
@@ -18,6 +22,7 @@ pub enum CliRecarCommand {
         #[clap(short, long)]
         yes: bool,
     },
+    Describe,
 }
 
 pub fn run(subcommand: CliRecarCommand, file: PathBuf) -> Result<()> {
@@ -35,17 +40,7 @@ pub fn run(subcommand: CliRecarCommand, file: PathBuf) -> Result<()> {
             }
         }
         CliRecarCommand::Check => {
-            let file_name = PathBuf::from(file.file_name().unwrap()).with_extension("");
-            let hash = file_name.to_str().unwrap();
-            let true_hash = format!("{:x}", generate::hash(&file)?);
-            let true_hash = true_hash.as_str();
-            if hash != true_hash {
-                bail!(miette!(
-                    "Hashes don't match!\nclaimed hash: {}\ntrue hash: {}",
-                    hash,
-                    true_hash,
-                ))
-            }
+            check(&file)?;
         }
         CliRecarCommand::Rename { yes } => {
             let new_file_name = format!("{:x}.recar", generate::hash(&file)?);
@@ -55,10 +50,7 @@ pub fn run(subcommand: CliRecarCommand, file: PathBuf) -> Result<()> {
             }
             if !yes {
                 if !Confirm::new()
-                    .with_prompt(format!(
-                        "Renaming {} to {}, do you want to continue?",
-                        file_name, new_file_name
-                    ))
+                    .with_prompt(format!("Rename {} to {}?", file_name, new_file_name))
                     .interact()
                     .map_err(|e| miette!("An error occurred when retrieving user input: {}", e))?
                 {
@@ -73,6 +65,32 @@ pub fn run(subcommand: CliRecarCommand, file: PathBuf) -> Result<()> {
             fs::rename(&file, &new_file)
                 .map_err(|e| miette!("An error occurred renaming this file: {}", e))?;
         }
+        CliRecarCommand::Describe => {
+            let hash = generate::hash(&file)?;
+            let size = File::open(&file).unwrap().metadata().unwrap().len();
+            let healthy = check(&file).is_ok();
+            println!("RECAR Archive:");
+            println!("path: {}", file.display());
+            println!("hash: {:x}", hash);
+            println!("size: {}", pretty_bytes::converter::convert(size as f64));
+            println!("healthy: {}", healthy)
+        }
+    }
+    Ok(())
+}
+
+fn check(file: impl AsRef<Path>) -> Result<()> {
+    let file = file.as_ref();
+    let file_name = PathBuf::from(file.file_name().unwrap()).with_extension("");
+    let hash = file_name.to_str().unwrap();
+    let true_hash = format!("{:x}", generate::hash(&file)?);
+    let true_hash = true_hash.as_str();
+    if hash != true_hash {
+        bail!(miette!(
+            "Hashes don't match!\nclaimed hash: {}\ntrue hash: {}",
+            hash,
+            true_hash,
+        ))
     }
     Ok(())
 }
