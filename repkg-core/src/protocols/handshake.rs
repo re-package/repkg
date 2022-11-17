5f211ea2abdc2376854710839a803ca70de12fd2
Tristan Fox <tristanfox009@outlook.com>
2022-11-17 14:09:41 +1100

    chore(reghost): add debug derivation to response

--- repkg-core/src/protocols/handshake.rs
+++ repkg-core/src/protocols/handshake.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize, Serialize)]
+#[derive(Deserialize, Serialize, Debug)]
 pub struct HandshakeResponse {
     pub(super) version: String,
 }
--- repkg-registry-host/src/api/handshake.rs
+++ repkg-registry-host/src/api/handshake.rs
@@ -1,8 +1,10 @@
 use axum::{http::StatusCode, response::IntoResponse, Json};
 use repkg_core::protocols::server as protocols;
+use tracing::info;
 
 pub async fn handshake() -> impl IntoResponse {
     let response = protocols::handshake::response();
+    info!("/api/handshake recieved, response: {:?}", response);
 
     (StatusCode::ACCEPTED, Json(response))
 }
